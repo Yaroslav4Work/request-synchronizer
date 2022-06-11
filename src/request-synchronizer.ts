ba838ecc7f7a39eb952b8781f98dea182a80661e
Yaroslav4Work <yaroslav4work@gmail.com>
2022-06-11 13:23:29 +0300

    Cosmetic changes
    Added:
    
    * Extended description in documentation;
    
    * The ability to handle errors through a callback.
    
    Changes:
    
    * Cosmetic change to a private property.

--- src/request-synchronizer.ts
+++ src/request-synchronizer.ts
@@ -5,27 +5,28 @@ export type RequestFunction = (req: Request, res: Response) => Promise<any>
 export interface RequestData {
   func: RequestFunction
   req: Request
-  res: Response,
+  res: Response
+  errCallback?: (err: any) => any
 }
 
 const DEFAULT = 'default'
 
 export default class RequestSynchronizer {
-  private static requestQueueGroups: {
+  private static _requestQueueGroups: {
     [index: string]: Promise<any>
   } = {
     default: new Promise((resolve) => resolve(null))
   }
 
   public static resolveRequest (reqData: RequestData, group = DEFAULT) {
-    if (!RequestSynchronizer.requestQueueGroups[group]) {
-      RequestSynchronizer.requestQueueGroups[group] = new Promise((resolve) => resolve(null))
+    if (!RequestSynchronizer._requestQueueGroups[group]) {
+      RequestSynchronizer._requestQueueGroups[group] = new Promise((resolve) => resolve(null))
     }
 
-    RequestSynchronizer.requestQueueGroups[group] = RequestSynchronizer.requestQueueGroups[group].then(
+    RequestSynchronizer._requestQueueGroups[group] = RequestSynchronizer._requestQueueGroups[group].then(
       () => reqData.func(reqData.req, reqData.res),
       (err) => {
-        console.log(`Error in queue: ${err}`)
+        reqData.errCallback ? reqData.errCallback(err) : console.log(`Error in queue: ${err}`)
         return new Promise((resolve) => resolve(null))
       }
     )
