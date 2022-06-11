275519212a7a0cacac1867b00f85388683f8d912
Yaroslav4Work <yaroslav4work@gmail.com>
2022-06-11 13:23:29 +0300

    Cosmetic changes
    Added:
    
    * Extended description in documentation;
    
    * The ability to handle errors through a callback.
    
    Changes:
    
    * Cosmetic change to a private property.

--- lib/request-synchronizer.d.ts
+++ lib/request-synchronizer.d.ts
@@ -1,15 +1,14 @@
 import { Request, Response } from 'express'
 
 export declare type RequestFunction = (req: Request, res: Response) => Promise<any>;
-
 export interface RequestData {
     func: RequestFunction;
     req: Request;
     res: Response;
+    errCallback?: (err: any) => any;
 }
-
 export default class RequestSynchronizer {
-    private static requestQueueGroups
+    private static _requestQueueGroups
 
     static resolveRequest (reqData: RequestData, group?: string): void;
 }
